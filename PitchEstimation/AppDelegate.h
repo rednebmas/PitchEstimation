052985861f40f1232fa7aa3ffb4deb799f1c560f
Sam Bender <rednebmas@gmail.com>
2015-11-10 18:57:15 -0800

    Initial Commit

---
+++ PitchEstimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PitchEstimation
+//
+//  Created by Sam Bender on 11/10/15.
+//  Copyright © 2015 Sam Bender. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
